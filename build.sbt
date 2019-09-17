36a651b4cdf0710104b2c7071cd00fd760b7f743
giorgiafiscaletti <gfisca2@uic.edu>
2019-09-17 14:49:48 -0500

    hello world

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Giorgia_Fiscaletti_hw1"
+
+version := "0.1"
+
+scalaVersion := "2.13.0"
---
+++ src/main/scala/com/gfisca2/Hello.scala
@@ -0,0 +1,7 @@
+package com.gfisca2
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
